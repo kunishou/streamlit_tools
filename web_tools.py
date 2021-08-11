c550f54e853c5469765a225565a45aa9ed1a91b0
kunishou <69422183+kunishou@users.noreply.github.com>
2021-08-11 10:22:58 +0900

    Update web_tools.py

--- web_tools.py
+++ web_tools.py
@@ -108,9 +108,8 @@ if sideradio == 'FAST PyCaret':
                 st.session_state.push2 = True
 
             if target and st.session_state.push2:
-                st.balloons()
                 st.write('使用メモリ上限の関係でこの先実行不可')
-                url2_2 = 'https://media.istockphoto.com/vectors/businessman-hands-holding-sorry-sign-vector-id500489798?k=6&m=500489798&s=612x612&w=0&h=9xfqUiTuqaGys5EMqQ7RiJQ2yJFmpl2D6P5xdcCOAXQ='
+                url2_2 = 'https://media.istockphoto.com/vectors/woman-says-sorry-sorry-face-vector-illustration-vector-id611877224?s=612x612'
                 image2_2 = Image.open(requests.get(url2_2,stream=True).raw).crop((100,200,1270,550))
                 st.image(image2_2,width=150)
 
@@ -151,14 +150,12 @@ if sideradio == 'Pandas-Profiling':
             st.write('')
             pr = df2.profile_report()          
             st_profile_report(pr)
-            st.balloons()
         if check2:
             df2 = pd.read_csv('titanic.csv')
             st.write(df2)
             pr = gen_profile_report(df2, explorative=True)
             with st.expander("DEMO REPORT", expanded=True):
                 st_profile_report(pr)
-                st.balloons()
 
 #-------------------------------------------------------------------------------------------------
 
